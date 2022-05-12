2e7db63267d7d569eda49394b202c77b54b44475
Vladislav <58528262+itotx@users.noreply.github.com>
2022-05-12 10:51:47 +0500

    Update prepare.sql
    not terminate currect pid

--- prepare.sql
+++ prepare.sql
@@ -1,4 +1,4 @@
-select pg_terminate_backend (pid) from pg_stat_activity where datname = '$PG_TO_DB';
+select pg_terminate_backend (pid) from pg_stat_activity where datname = '$PG_TO_DB' and pid <> pg_backend_pid();
 
 alter database $PG_TO_DB rename to "$PG_OLD_DB_NAME";
 
